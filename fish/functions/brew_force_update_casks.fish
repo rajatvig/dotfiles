34e5674925bf7320ad2933fba8f08020979892d8
Rajat Vig <Rajat.Vig@gmail.com>
2015-07-18 00:25:12 -0700

    more packages that supply latest and may not auto update

--- fish/functions/brew_force_update_casks.fish
+++ fish/functions/brew_force_update_casks.fish
@@ -1,8 +1,9 @@
 function brew_force_update_casks --description 'force update some casks that work odd'
-  for val in chromium google-chrome-canary google-drive skype
+  for val in chromium dash dropbox firefoxdeveloperedition flux google-chrome-canary google-chrome google-drive google-hangouts mou skype
     brew cask rm $val
-    rm -f /Library/Caches/Homebrew/$val-latest.dmg # google-chrome-canary google-drive skype
-    rm -f /Library/Caches/Homebrew/$val-latest # chromium
+    rm -f /Library/Caches/Homebrew/$val-latest.dmg
+    rm -f /Library/Caches/Homebrew/$val-latest.zip
+    rm -f /Library/Caches/Homebrew/$val-latest
     rm -rf /opt/homebrew-cask/Caskroom/$val
     brew cask install $val
   end
