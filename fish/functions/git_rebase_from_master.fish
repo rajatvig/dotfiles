2716e0b8b2e3f6ac5e7d6cd0e13938b574b19a66
Rajat Vig <rajat.vig@gmail.com>
2015-09-02 13:59:38 -0700

    new fish function for my gerrit workflow

---
+++ fish/functions/git_rebase_from_master.fish
@@ -0,0 +1,12 @@
+function git_rebase_from_master
+  function _git_branch_name
+    echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  end
+
+  set -l git_branch (_git_branch_name)
+
+  git checkout master
+  git pull --rebase origin master
+  git checkout $git_branch
+  git rebase master
+end
