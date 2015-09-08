f80276a4a51709a2b0c395752b7fccfe0374ec57
Rajat Vig <rajat.vig@gmail.com>
2015-09-08 10:52:08 -0700

    activate pyenv on projectile switch as anaconda is going haywire otherwise

--- config/spacemacs/elpy/packages.el
+++ config/spacemacs/elpy/packages.el
@@ -6,4 +6,8 @@
 (defun elpy/init-elpy ()
   (elpy-enable)
   (add-hook 'python-mode-hook 'elpy-mode)
+  (defun projectile-pyenv-mode-set ()
+    "Set pyenv version matching project name. Version must be already installed."
+    (pyenv-mode-set (projectile-project-name)))
+  (add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set)
   )
