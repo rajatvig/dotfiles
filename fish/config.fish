be32e4f78c95abcd29fa5785c620e87a8e014d84
Rajat Vig <rajat.vig@gmail.com>
2015-09-21 13:52:54 -0700

    use go

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,13 @@
 eval (direnv hook fish)
 
 set brew_rbenv "/usr/local/var/rbenv/shims"
+
 set -gx RBENV_ROOT /usr/local/var/rbenv
 
 set -gx PYENV_ROOT /usr/local/var/pyenv
 
-set -gx PATH /usr/local/sbin $PATH
+set -gx GOPATH /usr/local/var/go
+
+set -gx PATH /usr/local/sbin $PATH $GOPATH/bin
 
 # eval sh $HOME/.config/base16-shell/base16-default.dark.sh
