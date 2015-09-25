2091d29a2a24bb366421a67d79a49b536d8baba4
Rajat Vig <rajat.vig@gmail.com>
2015-09-25 10:22:36 -0700

    spacemacs is creaking odd with python

--- config/spacemacs/elpy/packages.el
+++ config/spacemacs/elpy/packages.el
@@ -8,6 +8,7 @@
   (add-hook 'python-mode-hook 'elpy-mode)
   (defun projectile-pyenv-mode-set ()
     "Set pyenv version matching project name. Version must be already installed."
+    (setenv PYTHONPATH (concat projectile-project-root ".tox/py27/bin"))
     (pyenv-mode-set (projectile-project-name)))
   (add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set)
   )
