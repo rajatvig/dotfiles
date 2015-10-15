4a9073eb1fbe84c17619790f0b23c5e2ca923901
Rajat Vig <rajat.vig@gmail.com>
2015-10-15 10:38:30 -0700

    more fish functions

--- fish/functions/brew_update_casks.fish
+++ fish/functions/brew_update_casks.fish
@@ -1,8 +1,7 @@
 function brew_update_casks --description 'update outdated casks'
   set -x _outdated_casks (brew_outdated_casks)
 
-  echo 'Outdated Casks'
-  echo $_outdated_casks
+  echo 'Outdated Casks $_outdated_casks'
 
   set -x _outdated_cask_names (echo $_outdated_casks | gsed "s/ /\n/g")
 
