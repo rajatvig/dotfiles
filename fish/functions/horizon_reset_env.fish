8d42ed4806ef10885ecfabe4b42dd6ac6a6d5c18
Rajat Vig <rajat.vig@gmail.com>
2015-12-21 14:46:40 -0800

    python comes via pyenv

--- fish/functions/horizon_reset_env.fish
+++ fish/functions/horizon_reset_env.fish
@@ -2,6 +2,7 @@ function horizon_reset_env
   rm -rf .tox .venv node_modules
   npm install
   rm -rf .tox
+  pip install tox
   tox -e py27 --notest
   tox -e py34 --notest
   source .tox/py27/bin/activate.fish
