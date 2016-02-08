9a6800e3a0b7fb00dda80da6c3825204264d7ff1
Rajat Vig <rajat.vig@gmail.com>
2016-02-08 11:36:27 -0800

    spacemacs updates; brew package updates; fish config changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH /Users/rajatv/.local/share/omf
+set -gx OMF_PATH ~/.local/share/omf
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/rajatv/.config/omf
+set -gx OMF_CONFIG ~/.config/omf
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
