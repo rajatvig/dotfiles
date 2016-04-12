c1e6b97133d9f3ab4f99e5249d6af7309aa0a99f
Rajat Vig <rajat.vig@gmail.com>
2016-04-12 09:34:22 -0700

    nvm possibly works best vs the rest

--- config/omf/before.init.fish
+++ config/omf/before.init.fish
@@ -5,8 +5,8 @@ set brew_rbenv "/usr/local/var/rbenv/shims"
 
 set -gx RBENV_ROOT /usr/local/var/rbenv
 set -gx PYENV_ROOT /usr/local/var/pyenv
-set -gx NVM_DIR /usr/local/var/nvm
 set -gx GOPATH /usr/local/var/go
+set -gx NVM_DIR /usr/local/var/nvm
 
 set -gx RUST_SRC_PATH $HOME/work/oss/rust/rust/src
 set -gx PATH $PATH $HOME/.cabal/bin $HOME/.multirust/toolchains/beta/cargo/bin
