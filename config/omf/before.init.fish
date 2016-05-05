81b20c9935922e5a9e4eede4f594710f9bfc2940
Rajat Vig <rajat.vig@gmail.com>
2016-05-04 23:50:18 -0700

    Fixed nvm; redid spacemacs

--- config/omf/before.init.fish
+++ config/omf/before.init.fish
@@ -8,5 +8,7 @@ set -gx PYENV_ROOT /usr/local/var/pyenv
 set -gx GOPATH /usr/local/var/go
 set -gx NVM_DIR /usr/local/var/nvm
 
+set -gx DOCKER_USER rajatvig
+
 set -gx RUST_SRC_PATH $HOME/work/oss/rust/rust/src
 set -gx PATH $PATH $HOME/.cabal/bin $HOME/.multirust/toolchains/beta/cargo/bin
