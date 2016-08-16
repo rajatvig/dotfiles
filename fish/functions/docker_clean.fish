5f3900026146124f88f3b2775d4801eb3f3636d4
Rajat Vig <rajat.vig@gmail.com>
2016-08-16 14:55:34 -0700

    clean up docker

--- fish/functions/docker_clean.fish
+++ fish/functions/docker_clean.fish
@@ -1,5 +1,7 @@
 function docker_clean
     docker rm (docker ps -aq)
+    docker rmi (docker images -aq)
     docker volume rm (docker volume ls -q)
+    docker network rm (docker network ls -q)
 end
 
