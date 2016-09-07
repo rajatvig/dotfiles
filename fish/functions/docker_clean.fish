528fdaa6780bd607945c149926aaeeee03c04d2b
Rajat Vig <rajat.vig@gmail.com>
2016-09-07 09:23:14 -0700

    fish function updates

--- fish/functions/docker_clean.fish
+++ fish/functions/docker_clean.fish
@@ -1,7 +1,6 @@
 function docker_clean
     docker rm (docker ps -aq)
-    docker rmi (docker images -aq)
+    docker rmi -f (docker images -aq)
     docker volume rm (docker volume ls -q)
     docker network rm (docker network ls -q)
 end
-
--- fish/functions/pip_upgrade.fish
+++ fish/functions/pip_upgrade.fish
@@ -1,4 +1,4 @@
 function pip_upgrade --description 'upgrade the pips; from http://stackoverflow.com/questions/2720014/upgrading-all-packages-with-pip'
-  pip install -U pip  
+  pip install -U pip
   pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 end
