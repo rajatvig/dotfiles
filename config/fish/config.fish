e0770329e096c6cf6938fb6724af470cffae8b31
Rajat Vig <rajatv@thoughtworks.com>
2017-12-03 20:22:35 -0800

    Fix ordering of includes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,4 @@
 direnv hook fish | source
-opam config env | source
 thefuck --alias | source
 
 set -gx EDITOR vim
@@ -11,10 +10,13 @@ set -g fish_user_paths /usr/local/sbin $fish_user_paths
 set -g fish_user_paths $HOME/Library/Haskell/bin $fish_user_paths
 set -g fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
 
+source /usr/local/opt/asdf/asdf.fish
+opam config env | source
+
 set tacklebox_path  ~/.config/fish_plugins/z ~/.config/fish_plugins/tackle ~/.config/fish_plugins/to ~/.config/fish_plugins/brew ~/.config/fish_plugins/pyenv ~/.config/fish_plugins/git
 set tacklebox_modules virtualfish z
 set tacklebox_plugins docker extract grc pip python
 
 source ~/.config/fish_plugins/tacklebox/tacklebox.fish
-source /usr/local/opt/asdf/asdf.fish
+
 source ~/.config/fish/private.fish
