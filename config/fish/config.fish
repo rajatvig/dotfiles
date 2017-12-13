313fb193523ef6fe515bbc73decc423f82d378b1
Rajat Vig <rajatv@thoughtworks.com>
2017-12-13 14:00:32 -0800

    Fix more of the arbitrary paths with asdf + atom

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,15 +10,17 @@ set -g fish_user_paths /usr/local/sbin $fish_user_paths
 set -g fish_user_paths $HOME/Library/Haskell/bin $fish_user_paths
 set -g fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
 
-source /usr/local/opt/asdf/asdf.fish
-opam config env | source
-
-set -g fish_user_paths (yarn global bin) $fish_user_paths
-
 set tacklebox_path  ~/.config/fish_plugins/z ~/.config/fish_plugins/tackle ~/.config/fish_plugins/to ~/.config/fish_plugins/brew ~/.config/fish_plugins/pyenv ~/.config/fish_plugins/git
-set tacklebox_modules virtualfish z
-set tacklebox_plugins docker extract grc pip python
+set tacklebox_modules z
+set tacklebox_plugins docker extract grc docker
 
 source ~/.config/fish_plugins/tacklebox/tacklebox.fish
+
+source /usr/local/opt/asdf/asdf.fish
+
+opam config env | source
+
+set -xg PATH (yarn global bin) $PATH
+set -xg PATH /usr/local/opt/asdf/installs/ruby/(asdf list ruby|head -n 1)/bin $PATH
 
 source ~/.config/fish/private.fish
