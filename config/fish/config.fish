5d6170b8198a5b9de1c98360e7b916f115abfa39
Rajat Vig <rajat.vig@gmail.com>
2018-01-26 13:42:27 -0800

    Misc Updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,6 @@ set -gx GOPATH /usr/local/var/go
 set -g fish_user_paths /usr/local/bin $fish_user_paths
 set -g fish_user_paths /usr/local/sbin $fish_user_paths
 set -g fish_user_paths $HOME/Library/Haskell/bin $fish_user_paths
-set -g fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
 
 set tacklebox_path  ~/.config/fish_plugins/z ~/.config/fish_plugins/tackle ~/.config/fish_plugins/to ~/.config/fish_plugins/brew ~/.config/fish_plugins/kubectl ~/.config/fish_plugins/git
 set tacklebox_modules z
