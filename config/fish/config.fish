dc1259f8be6d8fe13963724c1c27f221ffb31959
Rajat Vig <rajat.vig@gmail.com>
2018-11-01 00:07:58 -0700

    asdf path exports are awkward for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,13 +11,15 @@ set -g fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
 
 set -g fish_user_paths $GOPATH/bin $fish_user_paths
 
+set -l asdf_dir ~/.asdf
+set -g fish_user_paths $asdf_dir/bin $fish_user_paths
+set -g fish_user_paths $asdf_dir/shims $fish_user_paths
+
 set tacklebox_path ~/.config/fish_plugins/pure ~/.config/fish_plugins/z ~/.config/fish_plugins/tackle ~/.config/fish_plugins/to ~/.config/fish_plugins/brew ~/.config/fish_plugins/git ~/.config/fish_plugins/docker
 set tacklebox_modules z
 set tacklebox_plugins docker extract grc
 
 source ~/.config/fish_plugins/tacklebox/tacklebox.fish
-
-source ~/.asdf/asdf.fish
 
 for f in autotmux private
     source ~/.config/fish/$f.fish
